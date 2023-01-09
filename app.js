5e49043d18d1f0f7a1e7830228bcf311c277095d
ruhul <ra.01403288711@gmail.com>
2023-01-10 00:34:25 +0600

    modal location added & error solved

--- app.js
+++ app.js
@@ -27,10 +27,17 @@ app.get('/',(req,res)=>{
 
 app.get('/searchplot',(req,res)=>{
     const plot_no_en = req.query.plotNo;
-    var geoJson,stArea,stleng;
+    var geoJson,stArea,stleng,details;
     let query1 = "SELECT ST_AsGeoJson(ST_Simplify(geom,2)) FROM borolekh Where plot_no_en = $1";
     let query2 = "SELECT shape_area FROM borolekh WHERE plot_no_en = $1";
     let query3 = "SELECT shape_leng FROM borolekh WHERE plot_no_en = $1";
+    let query4 = "SELECT * FROM borolekh WHERE plot_no_en = $1";
+    pool.query(query4,[plot_no_en],(err,result)=>{
+        if(err)throw err;
+        details = result.rows[0]
+        
+    });
+   
     pool.query(query3,[plot_no_en],(err,result)=>{
         if(err)throw err;
         stleng= result.rows[0]
@@ -46,9 +53,24 @@ app.get('/searchplot',(req,res)=>{
     pool.query(query1,[plot_no_en],(err,results)=>{
         if(err)throw err;
         geoJson = results.rows[0].st_asgeojson; 
-        let sArea = parseFloat(stArea.shape_area*0.00002295684113).toFixed(4);
+        let sArea = parseFloat(stArea.shape_area)*0.00002295684113;
+        sArea = sArea.toFixed(4);
         let sLength = parseFloat(stleng.shape_leng)*3.28084 ;
-        res.render('map',{plotInfo:geoJson,plotArea:sArea,plotLeng:sLength.toFixed(2)});
+        
+        
+        
+        res.render('map',
+        {
+            plotInfo:geoJson,
+            plotArea:stArea.shape_area,
+            plotLeng:stleng.shape_leng,
+            district :details.m_dist_en,
+            subDistrict:details.m_name_en,
+            jlNo:details.jl_no_en,
+            plotNo:details.plot_no_en,
+           
+            
+        });
     });
   
     
