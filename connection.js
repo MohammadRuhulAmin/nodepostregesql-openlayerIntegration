49930b89bb80a6562db23667faf09709dcaf9cf4
ruhul <ra.01403288711@gmail.com>
2023-01-15 14:54:20 +0600

    db connected

--- connection.js
+++ connection.js
@@ -1,23 +1,23 @@
-const Pool = require('pg').Pool;
-const pool = new Pool({
-	user:"postgres",
-	host:"localhost",
-	database:"dynamic_image_processing",
-	//database:"shapeFile_database",
-	
-	password:"ruhulamin",
-	port:5432
-});
-
-module.exports = pool;
-
 // const Pool = require('pg').Pool;
 // const pool = new Pool({
-// 	user:"gisclousr",
-// 	host:"45.114.84.187",
-// 	database:"land_mouja_map",
-// 	password:"Gi!1oCuy67",
-// 	port:33341
+// 	user:"postgres",
+// 	host:"localhost",
+// 	database:"dynamic_image_processing",
+// 	//database:"shapeFile_database",
+	
+// 	password:"ruhulamin",
+// 	port:5432
 // });
 
 // module.exports = pool;
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+	user:"gisclousr",
+	host:"45.114.84.187",
+	database:"land_mouja_map",
+	password:"Gi!1oCuy67",
+	port:33341
+});
+
+module.exports = pool;
